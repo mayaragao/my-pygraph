3083cc7f35c66ab358d9aa1161f63ec9f9af94eb
joaohenrique <joaohenriquef@poli.ufrj.br>
2018-08-31 16:28:25 -0300

    File reading and parsing

--- src/graph.py
+++ src/graph.py
@@ -13,39 +13,47 @@ class Graph:
     @abstractmethod
     def add_node(self, v1, v2): pass
 
+    @abstractmethod
+    def _initialize_data_str(self): pass
+
     def __init__(self, input_path):
         input_file = open(input_path, "r")
-        num_vertices = input_file.readline()
+        self.num_vertices = int(input_file.readline())
+        print(self.num_vertices)
+        self._initialize_data_str()
         for line in input_file:
-            read_string = input_file.readline()
+            read_string = line
             relation_list = read_string.split(" ")
-            add_node(relation_list[0],relation_list[1])
-            num_edges += 1
-        # Each edge was count twice, so:
-        num_edges = num_edges / 2 
+            self.add_node(int(relation_list[0]),int(relation_list[1]))
+            self.num_edges += 1
  
     def generate_analisys(self, output_path):
-        output_file = open(input_path, "w")
-        output_file.write(f"Results of the {year} {event}")
+        output_file = open(output_path, "w")
+        output_file.write(f"Total vertices: {self.num_vertices}\n")
+        output_file.write(f"Total edges: {self.num_edges}\n")
+        output_file.write(f"Max degree: {self.max_degree}\n")
+        output_file.write(f"Min degree: {self.min_degree}\n")
+        output_file.write(f"Avg degree: {self.avg_degree}\n")
 
 class GraphList(Graph):
     adjacency_list = []
     
     def __init__(self, input_path):
-        super.__init__(input_path)
+        super().__init__(input_path)
 
-        for x in range(0, num_vertices):
-            adjacency_list[x] = SinglyLinkedList()
-            num_vertices =+ 1
-            if adjacency_list[x].num_itens > max_degree:
-                max_degree = adjacency_list[x].num_itens
-            elif adjacency_list[x].num_itens < min_degree
-                min_degree = adjacency_list.num_itens
-        avg_degree = num_edges / num_vertices
-
+    def _initialize_data_str(self):
+        print(self.num_vertices)
+        for _ in range(0, self.num_vertices):
+            self.adjacency_list.append(SinglyLinkedList())
 
     def add_node(self, v1, v2):
-        adjacency_list[v1].append(v2)
+        self.adjacency_list[v1].append(v2)
+        
+        if self.adjacency_list[v1].num_itens > self.max_degree:
+            self.max_degree = self.adjacency_list[v1].num_itens
+        elif self.adjacency_list[v1].num_itens < self.min_degree:
+            self.min_degree = self.adjacency_list[v1].num_itens
+        self.avg_degree = self.num_edges / self.num_vertices
 
 class GraphMatrix(Graph):
     def add_node(self):
--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-import graph
+from graph import GraphList
 
 def main():
-    
-    graph = GraphList()
+    graph = GraphList("../examples/as_graph.txt")
+    print(graph.num_vertices)
+    graph.generate_analisys("../output/as_graph_out.txt")
+
+main()
 
     
     
